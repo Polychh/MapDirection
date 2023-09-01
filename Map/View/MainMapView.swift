50ddd26fc9d8b15cb38ca2f65f87fccb1140c1a1
Polychh <polych1998@mail.ru>
2023-09-01 20:27:40 +0300

    Setting for constrains in separate file

---
+++ Map/View/MainMapView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMapView.swift
+//  Map
+//
+//  Created by USER on 30.08.2023.
+//
+
+import UIKit
+
+class MainMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
